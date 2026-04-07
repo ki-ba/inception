002139f4d1e702cfbb9e34de2d4b03bf634c3a51
kiba <shaper.vaquero-03@icloud.com>
2026-04-07 17:07:04 +0200

    omg wordpress

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-mkdir -p /var/www/html/wp
+mkdir -p /var/www/html/
 cd /var/www/html
 curl -O https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz -C wp
+tar -xzvf latest.tar.gz
 rm latest.tar.gz
 
-chown -R lighttpd /var/www/html
-ln -s /var/www/html/wp/ /var/www/localhost/htdocs/wordpress
-
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+ls -la > /var/www/html/wordpress-ls
 exec /usr/sbin/php-fpm82 -F
