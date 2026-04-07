0079d672333cc26242f60fa171d37efd586a3787
kiba <shaper.vaquero-03@icloud.com>
2026-04-07 19:21:27 +0200

    connect wordpress to mariadb

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,11 +1,47 @@
 #!/bin/sh
 set -e
 
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld /var/lib/mysql
+DATADIR="/var/lib/mysql"
+SOCKET="/run/mysqld/mysqld.sock"
 
-if [ ! -d /var/lib/mysql/mysql ]; then
-  mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+mkdir -p /run/mysqld "$DATADIR" /etc/my.cnf.d
+chown -R mysql:mysql /run/mysqld "$DATADIR"
+
+rm -f /etc/my.cnf /etc/mysql/my.cnf
+
+cat > /etc/my.cnf.d/docker.cnf << EOF
+[mysqld]
+bind-address=0.0.0.0
+port=3306
+datadir=/var/lib/mysql
+socket=/run/mysqld/mysqld.sock
+skip-networking=0
+EOF
+
+if [ ! -d "$DATADIR/mysql" ]; then
+    echo "Initializing MariaDB..."
+    mariadb-install-db --user=mysql --datadir="$DATADIR"
+
+    echo "Starting temporary MariaDB..."
+    mariadbd --user=mysql --datadir="$DATADIR" --socket="$SOCKET" --bind-address=0.0.0.0 --port=3306 &
+    pid="$!"
+
+    until mariadb --socket="$SOCKET" -e "SELECT 1" >/dev/null 2>&1; do
+        echo "Waiting for MariaDB..."
+        sleep 2
+    done
+
+    mariadb --socket="$SOCKET" -uroot <<EOSQL
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOSQL
+
+    mariadb-admin --socket="$SOCKET" -uroot -p"${MARIADB_ROOT_PASSWORD}" shutdown
+    wait "$pid" || true
 fi
 
-exec mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock
+echo "Starting MariaDB in foreground..."
+exec mariadbd --console --user=mysql --datadir="$DATADIR" --socket="$SOCKET" --bind-address=0.0.0.0 --port=3306
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
+set -e
 
-mkdir -p /var/www/html/
+mkdir -p /var/www/html
 cd /var/www/html
-curl -O https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
-rm latest.tar.gz
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-ls -la > /var/www/html/wordpress-ls
+if [ ! -d /var/www/html/wordpress ]; then
+  curl -LO https://wordpress.org/latest.tar.gz
+  tar -xzf latest.tar.gz
+  rm latest.tar.gz
+fi
+
+if [ ! -f /var/www/html/wp-cli.phar ]; then
+  curl -LO https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+fi
+
+chmod +x /var/www/html/wp-cli.phar
+
+until nc -z mariadb 3306; do
+  echo "Waiting for MariaDB..."
+  sleep 2
+done
+
+if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+  php /var/www/html/wp-cli.phar config create \
+    --path=/var/www/html/wordpress \
+    --dbname="$MARIADB_DATABASE" \
+    --dbuser="$MARIADB_USER" \
+    --dbpass="$MARIADB_PASSWORD" \
+    --dbhost="mariadb:3306" \
+    --skip-check \
+    --allow-root
+fi
+
 exec /usr/sbin/php-fpm82 -F
